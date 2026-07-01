968ac2b5c935188f0c213ce260b488e763bcf0e1
eduforci <ouattaramassa639@gmail.com>
2026-07-01 11:51:35 +0000

    Remove menu button functionality from script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const menuBtn = document.getElementById("menuBtn");
-const closeBtn = document.getElementById("closeBtn");
-const sideMenu = document.getElementById("sideMenu");
 
-menuBtn.onclick = () => {
-    sideMenu.classList.add("active");
-}
-
-closeBtn.onclick = () => {
-    sideMenu.classList.remove("active");
-}
