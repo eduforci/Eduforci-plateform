a042689096a4cbcdae64df6ff3fd97b34d24355a
eduforci <ouattaramassa639@gmail.com>
2026-07-05 07:22:38 +0000

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,7 +2,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-storage.js";
+
 // Configuration Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyD_9omHq9vzEsO0A7Owb2llJtDPYy3y2_8",
@@ -20,6 +21,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
+
 // Log une fois que tout est prêt
 console.log("Projet Firebase :", app.options.projectId);
 
